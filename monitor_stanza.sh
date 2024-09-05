d4fe09d724440d9da89977cd9c7b917c5470e9d0
kondepogu.mosesdayanand <kondepogu.mosesdayanand@gmail.com>
2024-09-05 05:41:48 +0000

    updated

--- monitor_stanza.sh
+++ monitor_stanza.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Create the file
-touch stanzas.conf
+touch inputs.conf
 
 # Write the stanzas to the file
 for i in {1..3000}; do
   echo "[monitor:///opt/3kfiles/file_$i.log]" >> stanzas.conf
-  echo "index = index_$i" >> stanzas.conf
+  echo "index = index_$i" >> inputs.conf
 done
